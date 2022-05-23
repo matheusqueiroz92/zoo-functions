42489a038889b943b54de3118bac0662734958a1
Matheus Queiroz <matheus_giga@hotmail.com>
2022-05-23 08:24:34 -0300

    Corrigindo o primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,6 +9,4 @@ function getSpeciesByIds(...ids) {
   });
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '89be95b3-47e4-4c5b-b687-1fabf2afa274'));
-
 module.exports = getSpeciesByIds;
