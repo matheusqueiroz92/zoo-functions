550ab499d9a44f1fede37a8829f7ee16c78f12d7
Matheus Queiroz <matheus_giga@hotmail.com>
2022-05-25 17:15:00 -0300

    Finalizando o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,20 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const allAnimals = {};
+    data.species.forEach((element) => {
+      (allAnimals[element.name] = element.residents.length);
+    });
+    return allAnimals;
+  }
+  const findAnimals = data.species.find((element) => element.name === animal.specie);
+  console.log(findAnimals);
+  if (animal.sex) {
+    const filterAnimals = findAnimals.residents.filter((element) => element.sex === animal.sex);
+    return filterAnimals.length;
+  }
+  return findAnimals.residents.length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,6 +1,4 @@
-// const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-// const msgError = throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 
 function isManager(id) {
   // seu código aqui
@@ -23,5 +21,5 @@ function getRelatedEmployees(managerId) {
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-// getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
+
 module.exports = { isManager, getRelatedEmployees };
