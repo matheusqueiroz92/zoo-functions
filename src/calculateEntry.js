0dc9603761a3baf53afe1167e66340871072d4d5
Matheus Queiroz <matheus_giga@hotmail.com>
2022-05-28 10:06:31 -0300

    Finalizando os requisitos 12 e 13

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,41 @@
 const data = require('../data/zoo_data');
 
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+
 function countEntrants(entrants) {
   // seu código aqui
+  if (!entrants) return 0;
+  const count = entrants.reduce((acc, cur) => {
+    if (cur.age < 18) acc.child += 1;
+    if (cur.age >= 18 && cur.age < 50) acc.adult += 1;
+    if (cur.age >= 50) acc.senior += 1;
+    return acc;
+  }, { adult: 0, child: 0, senior: 0 });
+  return count;
 }
-
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants) return 0;
+  if (Object.keys(entrants).length === 0) return 0;
+  // const pricesEntry = data.prices;
+  return entrants.reduce((acc, cur) => {
+    acc.adult *= count.adult;
+    return 
+  }, { adult: 49.99, senior: 24.99, child: 20.99 });
 }
-
+console.log(countEntrants([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]));
 module.exports = { calculateEntry, countEntrants };
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,43 @@
+const { it } = require('eslint/lib/rule-tester/rule-tester');
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {});
+it('Testando se getOpeningHours é uma função', () => {
+  expect(typeof getOpeningHours).toBe('function');
+});
+it('Testando se a função retorna um objeto se não receber nenhum parâmetro', () => {
+  expect(getOpeningHours()).toEqual({
+    Tuesday: { open: 8, close: 6 },
+    Wednesday: { open: 8, close: 6 },
+    Thursday: { open: 10, close: 8 },
+    Friday: { open: 10, close: 8 },
+    Saturday: { open: 8, close: 10 },
+    Sunday: { open: 8, close: 8 },
+    Monday: { open: 0, close: 0 },
+  });
+});
+it('Testando se a função retorna a string correta ao receber parâmetro1', () => {
+  expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+});
+it('Testando se a função retorna a string correta ao receber parâmetro2', () => {
+  expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+});
+it('Testando se a função retorna a string correta ao receber parâmetro3', () => {
+  expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+});
+it('Testando se a função ao receber uma string com a hora incorreta como parâmetro retorna um erro', () => {
+  const actual = '09:00-nn';
+  expect(() => getOpeningHours('Sunday', actual)).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+});
+it('Testando se a função ao receber uma string com o dia incorreto como parâmetro retorna um erro', () => {
+  const actual = 'quarta-feira';
+  expect(() => getOpeningHours(actual, '09:00-AM')).toThrow('The day must be valid. Example: Monday');
+});
+it('Testando se a função ao receber uma string com o hora incorreta como parâmetro retorna um erro', () => {
+  const actual = '29:55-AM';
+  expect(() => getOpeningHours('Monday', actual)).toThrow('The hour must be between 0 and 12');
+});
+it('Testando se a função ao receber uma string com o hora incorreta como parâmetro retorna um erro', () => {
+  const actual = '08:72-AM';
+  expect(() => getOpeningHours('Monday', actual)).toThrow('The minutes must be between 0 and 59');
+});
