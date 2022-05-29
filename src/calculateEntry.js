79b9a3e371a83f566603db6163126c6b0ae51391
Matheus Queiroz <matheus_giga@hotmail.com>
2022-05-29 01:58:18 -0300

    Finalizando o requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,13 +1,13 @@
 const data = require('../data/zoo_data');
 
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
+// const arr = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
 
 // const entries = Object.entries(arr);
 // const keys = Object.keys(arr);
@@ -16,47 +16,64 @@ const entrants = [
 // console.log(entries);
 // console.log(keys);
 // console.log(values);
-const count1 = entrants.reduce((acc, cur) => {
-  if (cur.age < 18) acc.cchild += 1;
-  return acc;
-}, { adult: 0, child: 0, senior: 0 });
 
-const count2 = entrants.reduce((acc, cur) => {
-  if (cur.age >= 18 && cur.age < 50) acc.cchild += 1;
-  return acc;
-}, { adult: 0, child: 0, senior: 0 });
-
-const count3 = entrants.reduce((acc, cur) => {
-  if (cur.age >= 18 && cur.age < 50) acc.cchild += 1;
-  return acc;
-}, { adult: 0, child: 0, senior: 0 });
+const count1 = (acc1, cur1) => {
+  const obj1 = acc1;
+  if (cur1.age < 18) {
+    obj1.child += 1;
+  }
+};
+const count2 = (acc2, cur2) => {
+  const obj2 = acc2;
+  if (cur2.age >= 18 && cur2.age < 50) {
+    obj2.adult += 1;
+  }
+};
+const count3 = (acc3, cur3) => {
+  const obj3 = acc3;
+  if (cur3.age >= 50) {
+    obj3.senior += 1;
+  }
+};
 
 function countEntrants(entrants) {
   // seu código aqui
   if (!entrants) return 0;
   const count = entrants.reduce((acc, cur) => {
     // if (cur.age < 18) acc.child += 1;
-    count1;
+    count1(acc, cur);
     // if (cur.age >= 18 && cur.age < 50) acc.adult += 1;
-    count2;
+    count2(acc, cur);
     // if (cur.age >= 50) acc.senior += 1;
-    count3;
+    count3(acc, cur);
     return acc;
   }, { adult: 0, child: 0, senior: 0 });
   return count;
 }
 function calculateEntry(entrants) {
   // seu código aqui
-  // if (!entrants) return 0;
-  // if (Object.keys(entrants).length === 0) return 0;
-  // const pricesEntry = [...count];
-  // console.log(pricesEntry);
-  // return entrants.reduce((acc, cur) => {
-  //   acc.adult *= count.adult;
-  //   return ;
-  // }, { adult: 49.99, senior: 24.99, child: 20.99 });
+  if (!entrants) return 0;
+  if (Object.keys(entrants).length === 0) return 0;
+  const pricesEntry = countEntrants(entrants);
+  const openObj = [pricesEntry];
+  const calculate = openObj.reduce((acc, cur) => {
+    acc.adult *= cur.adult;
+    acc.child *= cur.child;
+    acc.senior *= cur.senior;
+    return acc;
+  }, { adult: 49.99, senior: 24.99, child: 20.99 });
+  const result = Object.values(calculate);
+  return result.reduce((acc, cur) => acc + cur);
 }
 console.log(countEntrants([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]));
+console.log(calculateEntry([
   { name: 'Lara Carvalho', age: 5 },
   { name: 'Frederico Moreira', age: 5 },
   { name: 'Pedro Henrique Carvalho', age: 5 },
