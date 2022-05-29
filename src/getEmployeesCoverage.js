4d155f1681e879b7c094a0b8ba40eb4e8929f45b
Matheus Queiroz <matheus_giga@hotmail.com>
2022-05-29 19:10:19 -0300

    Finalizando o requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,11 +1,40 @@
 const data = require('../data/zoo_data');
 
+// recebe o nome da espécie e retorna a sua localização 
+function getSpecieByLocation(animal) {
+  return (data.species.find((element) => element.name === animal.name)).location;
+}
+console.log(getSpecieByLocation({ name: 'penguins' }));
+
+// recebe o nome do funcionário e retorna os animais que ele é responsável
+const speciesResponsible = (nameEmployee) => data.employees
+  .find((element) => element.firstName === nameEmployee.name).responsibleFor;
+function getEmployeeResponsibleFor(nameEmployee) {
+  return (speciesResponsible(nameEmployee));
+}
+console.log(getEmployeeResponsibleFor({ name: 'Nigel' }));
+
+const getSpecie = (employee) => {
+  const animal = employee.responsibleFor;
+  const nameAnimals = animal.filter((id) => data.species.find((element) => element.id === employee.id));
+  return nameAnimals;
+}
+
+const mapEmployeesFirst = data.employees.map((element) => element.firstName);
+const mapEmployeesLast = data.employees.map((element) => element.lastName);
+const mapEmployeesId = data.employees.map((element) => element.id);
+
 function getEmployeesCoverage(objFound) {
   // seu código aqui
-  const founds = data.employees.map((element) => element.firstName);
+  const allEmployees = data.employees.map((element) => element);
+  // const { id, fullName, species, locations } = data.employees[objFound];
   if (!objFound) {
-    return founds;
+    return allEmployees;
+  }
+  if (mapEmployeesFirst.includes(objFound.name) || mapEmployeesLast.includes(objFound)
+    || mapEmployeesId.includes(objFound.id)) {
+    return getSpecie(objFound);
   }
 }
-console.log(getEmployeesCoverage());
+console.log(getEmployeesCoverage('Nelson'));
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
+// function getOldestFromFirstSpecies(id) {
+//   // seu código aqui
+//   return data.employees.find((element) => element.id === id);
+// }
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+
+function getAnimalResponsable(id) {
+  return data.employees.find((employee) => employee.id === id).responsibleFor[0];
+}
+
+function findGroupAnimal(id) {
+  return data.species.find((specie) => specie.id === getAnimalResponsable(id));
+}
+
+function getAllAnimal(id) {
+  const array = [];
+  findGroupAnimal(id).residents.forEach((element) => array.push(element));
+  return array;
+}
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const oldest = getAllAnimal(id).sort((animalA, animalB) => animalB.age - animalA.age)[0];
+  const { name, sex, age } = oldest;
+  return [name, sex, age];
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
