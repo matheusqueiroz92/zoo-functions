800312e33ea39631dc423ed432311eda12a5b2a4
Matheus Queiroz <matheus_giga@hotmail.com>
2022-05-29 16:07:21 -0300

    Finalizando o requisito 9

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,22 +1,5 @@
 const data = require('../data/zoo_data');
 
-// const arr = [
-//   { name: 'Lara Carvalho', age: 5 },
-//   { name: 'Frederico Moreira', age: 5 },
-//   { name: 'Pedro Henrique Carvalho', age: 5 },
-//   { name: 'Maria Costa', age: 18 },
-//   { name: 'Núbia Souza', age: 18 },
-//   { name: 'Carlos Nogueira', age: 50 },
-// ];
-
-// const entries = Object.entries(arr);
-// const keys = Object.keys(arr);
-// const values = Object.values(arr);
-
-// console.log(entries);
-// console.log(keys);
-// console.log(values);
-
 const count1 = (acc1, cur1) => {
   const obj1 = acc1;
   if (cur1.age < 18) {
@@ -40,16 +23,14 @@ function countEntrants(entrants) {
   // seu código aqui
   if (!entrants) return 0;
   const count = entrants.reduce((acc, cur) => {
-    // if (cur.age < 18) acc.child += 1;
     count1(acc, cur);
-    // if (cur.age >= 18 && cur.age < 50) acc.adult += 1;
     count2(acc, cur);
-    // if (cur.age >= 50) acc.senior += 1;
     count3(acc, cur);
     return acc;
   }, { adult: 0, child: 0, senior: 0 });
   return count;
 }
+
 function calculateEntry(entrants) {
   // seu código aqui
   if (!entrants) return 0;
@@ -65,20 +46,5 @@ function calculateEntry(entrants) {
   const result = Object.values(calculate);
   return result.reduce((acc, cur) => acc + cur);
 }
-console.log(countEntrants([
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-]));
-console.log(calculateEntry([
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-]));
+
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
 
+const cronograma = {
+  Tuesday: { officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'] },
+  Wednesday: { officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'] },
+  Thursday: { officeHour: 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'] },
+  Friday: { officeHour: 'Open from 10am until 8pm',
+    exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'] },
+  Saturday: { officeHour: 'Open from 8am until 10pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'otters', 'frogs',
+      'snakes', 'elephants'] },
+  Sunday: { officeHour: 'Open from 8am until 8pm',
+    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'] },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+
+const days = Object.keys(cronograma);
+const mapAnimals = data.species.map((element) => element.name);
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (mapAnimals.includes(scheduleTarget)) {
+    return (data.species.find((element) => scheduleTarget === element.name)).availability;
+  }
+  if (!scheduleTarget || !days.includes(scheduleTarget)) return cronograma;
+  if (scheduleTarget === 'Monday') {
+    const objMonday = { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+    return objMonday;
+  }
+  const { open, close } = data.hours[scheduleTarget];
+  const disponibility = `Open from ${open}am until ${close}pm`;
+  const exhibition = data.species.filter((element) => element.availability
+    .includes(scheduleTarget)).map((animal) => animal.name);
+  const objDay = {};
+  objDay[scheduleTarget] = { officeHour: disponibility, exhibition };
+  return objDay;
 }
 
 module.exports = getSchedule;
